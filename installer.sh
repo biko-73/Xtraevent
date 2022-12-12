8cf24c50e2cc215ff47d653e612b1e8bc7fe6daf
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-12-12 17:01:11 +0200

    v_3.3

--- installer.sh
+++ installer.sh
@@ -13,7 +13,6 @@
 PACKAGE_DIR='Xtraevent/main'
 
 MY_IPK="enigma2-plugin-extensions-xtraevent_v3.3_all.ipk"
-MY_DEB=""
 
 ########################################################################################################################
 # Auto ... Do not change
@@ -22,9 +21,6 @@ MY_DEB=""
 # Decide : which package ?
 MY_MAIN_URL="https://raw.githubusercontent.com/biko-73/"
 if which dpkg > /dev/null 2>&1; then
-	MY_FILE=$MY_DEB
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
-else
 	MY_FILE=$MY_IPK
 	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
 fi
